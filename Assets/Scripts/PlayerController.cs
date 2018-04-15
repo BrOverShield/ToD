21561b53e8195f56122d970c1636e56b20c58d6e
ppouch <alexandre.lessard.2@ens.etsmtl.ca>
2018-04-15 03:01:16 -0400

    poke animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,12 +39,12 @@ public class PlayerController : MonoBehaviour
 		}
 
 
-        if(Vector3.Distance(this.transform.position,pickuper.transform.position)<=0.2)
-        {
-            pickuper.SetActive(false);
-            count++;
-            setCountText();
-        }
+        //if(Vector3.Distance(this.transform.position,pickuper.transform.position)<=0.2)
+        //{
+            //pickuper.SetActive(false);
+            //count++;
+            //setCountText();
+        //}
 	}
 
 	void OnTriggerEnter(Collider other)
@@ -52,6 +52,12 @@ public class PlayerController : MonoBehaviour
 		if (other.gameObject.CompareTag("Pick Up"))
 		{
             pickit(other);
+		}
+		else if (other.gameObject.CompareTag("Trap"))
+		{
+			other.gameObject.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
+			other.gameObject.tag = "Untagged";
+			Debug.Log("touche");
 		}
 	}
     void pickit(Collider other)
