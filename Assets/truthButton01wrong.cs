536f0b5dc2404eb35ac73cc4f4ffc9e70d28aaea
ppouch <alexandre.lessard.2@ens.etsmtl.ca>
2018-04-15 02:39:30 -0400

    menus
    asd

---
+++ Assets/truthButton01wrong.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class truthButton01wrong : MonoBehaviour {
+
+	public Button yourButton;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(changemenuscene);
+	}
+	
+	public void changemenuscene()
+	{
+		SceneManager.LoadScene("Truth01-wrong");
+		Debug.Log("01Wrong!");
+	}
+}
